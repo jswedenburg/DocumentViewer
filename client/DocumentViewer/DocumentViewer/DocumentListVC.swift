eefdfaee1f55d39795eb5ab6144693467e9be707
Jake Swedenburg <jswedenburg@gmail.com>
2018-04-15 15:44:21 -0600

    Set up tableview

--- client/DocumentViewer/DocumentViewer/DocumentListVC.swift
+++ client/DocumentViewer/DocumentViewer/DocumentListVC.swift
@@ -10,23 +10,53 @@ import UIKit
 
 class DocumentListVC: UIViewController {
 
+    @IBOutlet weak var docTableView: UITableView!
+    
     var documents:[Document] = []
+    let cellId:String = "documentCell"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         getDocuments()
+        setUpTableview()
+        setupUI()
+    }
+    
+    func setupUI() {
+        //NavBar
+        if let navFont = UIFont(name: "Roboto-Bold", size: 24) {
+            self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.font : navFont]
+        }
+    }
+    
+    func setUpTableview() {
+        docTableView.delegate = self
+        docTableView.dataSource = self
     }
     
     func getDocuments() {
         Document.getAllDocuments { (error, docs) in
             if let docs = docs {
-                self.documents = docs
+                DispatchQueue.main.async {
+                    self.documents = docs
+                    self.docTableView.reloadData()
+                }
             }
         }
     }
+}
 
+extension DocumentListVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return documents.count
+    }
     
-
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = docTableView.dequeueReusableCell(withIdentifier: cellId) as? DocumentTableViewCell else { return UITableViewCell() }
+        let doc = documents[indexPath.row]
+        cell.updateWith(doc: doc)
+        return cell
+    }
 }
 
---
+++ client/DocumentViewer/DocumentViewer/DocumentTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  DocumentCellTableViewCell.swift
+//  DocumentViewer
+//
+//  Created by Jake Swedenburg on 4/15/18.
+//  Copyright © 2018 Jake Swedenburg. All rights reserved.
+//
+
+import UIKit
+
+class DocumentTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imgPreview: UIImageView!
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblFileSize: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func updateWith(doc: Document) {
+        lblName.text = doc.name
+        lblFileSize.text = doc.size
+        switch doc.type {
+        case .image:
+            imgPreview.image = #imageLiteral(resourceName: "ic_pic")
+        case .pdf:
+            imgPreview.image = #imageLiteral(resourceName: "ic_pdf")
+        }
+    }
+
+    
+
+}
